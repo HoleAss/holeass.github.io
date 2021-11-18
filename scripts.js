50c3d74f16b7f5ec698e0becdedf84157a6abbca
av.sheremetev <av.sheremetev@tensor.ru>
2021-11-18 22:59:24 +0300

    json

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function exportList() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open('GET', '/resources/people.json', true);
+    xhttp.send();
+}
+
+button.addEventListener('click', exportList.bind(this));
