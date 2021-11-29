d9ee55b77acef63a1d62c79647db7db6f4c4726e
av.sheremetev <av.sheremetev@tensor.ru>
2021-11-29 18:49:23 +0300

    task

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { Tasks } from "./components/Tasks/Tasks";
-import { statusesList } from "./mocks/Statuses";
-import "./styles/main.scss";
-
-document.addEventListener("DOMContentLoaded", function (event) {
-  const tasks = new Tasks();
-  tasks.render();
-});
